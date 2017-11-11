cb4b5a70e85f9b2483a9d302e933a2bfdb2aa13c
Alex Supkay <asupkay1124@gmail.com>
2017-11-10 20:23:44 -0500

    MVP of cloud function

--- gcf-random-picker/index.js
+++ gcf-random-picker/index.js
@@ -5,14 +5,18 @@
  * @param {Object} res Cloud Function response context.
  */
 exports.randomGET = function randomGET (req, res) {
-    let emails = req.body;
+    let emails = req.body.emails;
+    console.log(emails);
     let randomEmail = getRandomEmail(emails);
+    console.log(randomEmail);
     res.send(randomEmail);
 };
 
 let getRandomEmail = (emails) => {
-    let emailArray = emails.split('\n');
-    let randomIndex = Math.floor(Math.random() * emails.length);
+    let emailArray = emails.split(' ');
+    console.log(emailArray);
+    let randomIndex = Math.floor(Math.random() * emailArray.length);
+    console.log(randomIndex);
 
-    return emails[randomIndex];
+    return emailArray[randomIndex];
 }

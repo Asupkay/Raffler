20e17ec2ab61f396a3f2dfeb96a139347b4a24cd
Alex Supkay <asupkay1124@gmail.com>
2017-11-17 15:03:35 -0500

    Implement full http to display linework

--- raffle-site/routes/raffler.js
+++ raffle-site/routes/raffler.js
@@ -3,9 +3,10 @@ const https = require('https');
 const router = express.Router();
 
 router.post("/", (req, res) => {
-    let data = JSON.stringify({emails: req.body.emails});
+    let emails = req.body.emails;
+    let data = JSON.stringify({emails: emails});
 
-    var options = {
+    const options = {
         host: 'us-central1-raffler-185618.cloudfunctions.net',
         path: '/randomGET',
         method: 'POST',
@@ -16,18 +17,20 @@ router.post("/", (req, res) => {
         
     };
 
-   var req = https.request(options, function(res) {
-        res.setEncoding('utf-8');
-        res.on('data', function (chunk) {
-            console.log("body: " + chunk);
+    var req = https.request(options, function(resT) {
+        resT.setEncoding('utf-8');
+        let fullRes = "";
+        resT.on('data', function (chunk) {
+            fullRes += chunk;
+        });
+        resT.on('end', function() {
+            let winner = encodeURIComponent(fullRes);
+            res.redirect('/?winner=' + winner + '&emails=' + emails);
         });
     });
 
     req.write(data);
     req.end();    
-
-    //var winner = encodeURIComponent('Success');
-    //res.redirect('/?winner=' + winner + '&emails=' + );
 });
 
 module.exports = router;

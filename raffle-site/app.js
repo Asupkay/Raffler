ca24e77408a9a58bd9c272eb42532f9649ffe885
Alex Supkay <asupkay1124@gmail.com>
2017-11-17 17:38:28 -0500

    Made app compatable with launching on app engine

--- raffle-site/app.js
+++ raffle-site/app.js
@@ -13,6 +13,7 @@ app.set('view engine', 'handlebars');
 
 configRoutes(app);
 
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Server running on http://localhost:" + PORT);
 });

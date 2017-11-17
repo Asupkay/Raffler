59410e35ccbe5e7fa04b82dae774090db13d2a38
Alex Supkay <asupkay1124@gmail.com>
2017-11-17 14:49:57 -0500

    Start hitting route hangs because no response is sent back

--- raffle-site/routes/raffler.js
+++ raffle-site/routes/raffler.js
@@ -1,9 +1,33 @@
 const express = require("express");
+const https = require('https');
 const router = express.Router();
 
 router.post("/", (req, res) => {
-    var winner = encodeURIComponent('Success');
-    res.redirect('/?winner=' + winner + '&emails=' + req.body.emails);
+    let data = JSON.stringify({emails: req.body.emails});
+
+    var options = {
+        host: 'us-central1-raffler-185618.cloudfunctions.net',
+        path: '/randomGET',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(data)
+        }
+        
+    };
+
+   var req = https.request(options, function(res) {
+        res.setEncoding('utf-8');
+        res.on('data', function (chunk) {
+            console.log("body: " + chunk);
+        });
+    });
+
+    req.write(data);
+    req.end();    
+
+    //var winner = encodeURIComponent('Success');
+    //res.redirect('/?winner=' + winner + '&emails=' + );
 });
 
 module.exports = router;
